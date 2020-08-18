26ecf50357d6de235fba1c6f0aaed65286343e17
vinothini-dhanasekar <vinothinidhanasekar0@gmail.com>
2020-08-18 09:42:04 +0530

    before

---
+++ Employee.java
@@ -0,0 +1,38 @@
+class Employee
+    {
+        public string name;
+        public int salary;
+        public int joiningDate;
+
+        public Employee()
+        {
+
+        }
+
+        public Employee(string name, int salary, int joiningDate)
+        {
+            this.name = name;
+            this.salary = salary;
+            this.joiningDate = joiningDate;
+        }
+
+        public void GetEmployeeData()
+        {
+            {
+                Console.WriteLine("Enter the name of employee: ");
+                name = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("Enter the salary of employee: ");
+                salary = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the date of joining of employee: ");
+                joiningDate = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        public void DisplayEmployee()
+        {
+            Console.WriteLine("The name of employee is: "+ name);
+            Console.WriteLine("The salary of employee is: "+salary);
+            Console.WriteLine("The date of joining of employee is: "+joiningDate);
+        }
+    }
+	}
---
+++ TestEmployee.java
@@ -0,0 +1,21 @@
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] e = new Employee[10];
+            for(int i = 0; i < 10; i++)
+            {
+                 Console.WriteLine($"Enter the data of employee with id: {i} *****************************************");
+                e[i] = new Employee();
+                e[i].GetEmployeeData();
+            }
+
+            Console.WriteLine("***********The data of given employees is: ");
+
+            for(int i = 0; i < 10; i++)
+            {
+                e[i].DisplayEmployee();
+            }
+        }
+    }
+
